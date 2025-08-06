1b654933d175f4377bffa12a3043c7c0e14bbc4c
carloslozano-rg <clozanoguadarrama@gmail.com>
2025-08-05 19:41:49 -0600

    Agrega funcionalidad de reset a función 'sortearAmigo' y mejora comentarios

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
 // Declara una variable de tipo array, que almacenará los nombres de los amigos ingresados.
 let amigos = [];
 
-// Crea una función para limpiar la caja de texto.
+// Crea una función para limpiar la única caja de texto.
 function limpiarCaja() {
     let valorCaja = document.querySelector("#amigo");
     valorCaja.value = "";
 }
 
-// Agrega la lista de amigos debajo de la caja de texto, en forma de una sola cadena de texto.
+// Crea una función para limpiar listas.
+function limpiarLista(id) {
+    let listaLimpia = document.getElementById(id);
+    listaLimpia.innerHTML = "";
+}
+
+// Agrega la lista 'amigos' a la lista desordenada 'listaAmigos', en forma de una sola cadena de texto.
+// Es una alternativa a 'listaAmigosLi', no se uso.
 function listaAmigosCadena() {
     amigosCadena = "";
     for (let index in amigos) {
@@ -21,20 +26,19 @@ function listaAmigosCadena() {
     document.getElementById("listaAmigos").innerHTML = amigosCadena;
 }
 
-// Agrega la lista de amigos debajo de la caja de texto, creando un nuevo list item por cada amigo.
+// Agrega la lista 'amigos' a la lista desordenada 'listaAmigos', creando un list item por cada elemento de 'amigos'.
 function listaAmigosLi() {
     let listaDesordenada = document.getElementById("listaAmigos");  
     listaDesordenada.innerHTML = "";
-    // Debido a que en Chrome solo muestra el estado final de la 'ul' cuando solo ingresamos 'console.log(listaDesordenada);', se tiene que llamar a un atributo.
     // console.log("Lista limpia: ",listaDesordenada.textContent);
 
+    // Crea un nuevo 'li' por cada amigo.
     for (let amigo of amigos) {
-        // Crea un nuevo 'li' por cada amigo.
         let listaElemento = document.createElement("li");
         listaElemento.textContent = amigo;
         listaDesordenada.appendChild(listaElemento);
     }
-    // console.log("Lista completa: ", listaDesordenada.textContent)
+    // console.log("Lista completa: ", listaDesordenada.textContent);
 }
 
 // Crea una función para agregar amigos.
@@ -52,24 +56,29 @@ function agregarAmigo() {
         // Cualquiera de los 2 métodos funciona, pero 'listaAmigosLi()' cumple con los requisitos del challenge.
         // listaAmigosCadena();
         listaAmigosLi();
+        // En caso de que ya obtuviesemos un 'resultado' previo.
+        limpiarLista("resultado");
     }
 }
 
+// Función para escoger un amigo secreto.
 function sortearAmigo() {
     numeroAmigos = amigos.length;
     if (numeroAmigos === 0) {
+        // Si la lista 'amigos' esta vacía.
         alert("Por favor, inserte un nombre.");
     } else {
-        document.getElementById("listaAmigos").innerHTML = "";
-        
+        // Si no, se elige un elemento de dicha lista.  
         amigoSecretoIndex = Math.floor(Math.random() * numeroAmigos);
-        console.log(amigoSecretoIndex);
-        console.log(amigos);
-        
+        // console.log(amigoSecretoIndex);
+        // console.log(amigos);
         amigoSecreto = amigos[amigoSecretoIndex];
         
         let resultado = document.getElementById("resultado");
-        resultado.innerHTML = amigoSecreto; 
+        resultado.innerHTML = amigoSecreto;
+        
+        // Se reinician las listas, para que el botón de 'Sortear amigo' sirva también como reset.
+        limpiarLista("listaAmigos"); 
+        amigos = [];
     }
 }
-
